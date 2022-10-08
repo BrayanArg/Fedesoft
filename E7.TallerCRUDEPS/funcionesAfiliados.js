cc5728ff075b9bb2f1f495c403f0964eeaefd344
BrayanArg <brayanalexisruiz@gmail.com>
2022-10-07 22:40:03 -0500

    Se creó nuevo proyecto CRUDEPS y se añadió HTML y JS

---
+++ E7.TallerCRUDEPS/funcionesAfiliados.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+  $("#tabla-afiliados").DataTable();
+});
+
+document.getElementById("formulario").addEventListener("submit", crear);
+let numActualizar ;
+
+function crear(e) {
+  let id = document.getElementById("inputId").value;
+  let num = document.getElementById("inputNum").value;
+  let nombre = document.getElementById("inputNombre").value;
+  let fecha = document.getElementById("inputFecha").value;
+  let esp = document.getElementById("inputEsp").value;
+
+  let paciente = {
+    id,
+    num,
+    nombre,
+    fecha,
+    esp,
+  };
+
+  if (localStorage.getItem("afiliados") === null) {
+    let pacientes = [];
+    pacientes.push(paciente);
+    localStorage.setItem("afiliados", JSON.stringify(pacientes));
+  } else {
+    let pacientes = JSON.parse(localStorage.getItem("afiliados"));
+    let elementIndex = pacientes.findIndex((obj) => obj.num == num);
+    if (elementIndex == -1) {
+      pacientes.push(paciente);
+      localStorage.setItem("afiliados", JSON.stringify(pacientes));
+    } else {
+      alert(
+        "Usuario existente, por favor digite otro númedo de identificación."
+      );
+    }
+  }
+
+  leer();
+  document.getElementById("formulario").reset();
+  e.preventDefault();
+
+  console.log("Afiliado Guardado Correctamente");
+}
+
+function leer() {
+  let pacientes = JSON.parse(localStorage.getItem("afiliados"));
+  //   console.log(pacientes);
+  if (pacientes != null) {
+    document.getElementById("tabla-tbody").innerHTML = "";
+    for (let i = 0; i < pacientes.length; i++) {
+      let id = pacientes[i].id;
+      let num = pacientes[i].num;
+      let nombre = pacientes[i].nombre;
+      let fecha = pacientes[i].fecha;
+      let esp = pacientes[i].esp;
+
+      document.getElementById("tabla-tbody").innerHTML += `<tr>
+          <td>${id}</td>
+          <td>${num}</td>
+          <td>${nombre}</td>
+          <td>${fecha}</td>
+          <td>${esp}</td>
+          <td><button class="btn btn-success" onclick="editar('${num}')" data-bs-toggle="modal"
+          data-bs-target="#modalEdit">Editar</button></td>
+          <td><button class="btn btn-danger" onclick="eliminar('${num}')">Eliminar</button></td>
+          `;
+    }
+  }
+}
+leer();
+
+function editar(num){
+    let pacientes = JSON.parse(localStorage.getItem("afiliados"));
+    let elementIndex = pacientes.findIndex((obj) => obj.num == num);
+    
+    document.getElementById("inputIdA").value= pacientes[elementIndex].id;
+    document.getElementById("inputNumA").value= pacientes[elementIndex].num;
+    document.getElementById("inputNombreA").value= pacientes[elementIndex].nombre;
+    document.getElementById("inputFechaA").value= pacientes[elementIndex].fecha;
+    document.getElementById("inputEspA").value= pacientes[elementIndex].esp;
+    numActualizar = num;
+}
+
+function actualizar(){
+    // debugger
+    let pacientes = JSON.parse(localStorage.getItem("afiliados"));
+    let elementIndex = pacientes.findIndex((obj) => obj.num == numActualizar);
+    // console.log(elementIndex);
+    
+    pacientes[elementIndex].nombre = document.getElementById("inputNombreA").value;
+    pacientes[elementIndex].fecha = document.getElementById("inputFechaA").value;
+    pacientes[elementIndex].esp = document.getElementById("inputEspA").value;
+    localStorage.setItem("afiliados", JSON.stringify(pacientes));
+    alert("Usuario actualizado exitosamente!");
+    leer();
+}
+
+function eliminar(){
+    let pacientes = JSON.parse(localStorage.getItem("afiliados"));
+    let elementIndex = pacientes.findIndex((obj) => obj.num == numActualizar);
+
+    pacientes.splice(elementIndex, 1);
+    localStorage.setItem("afiliados", JSON.stringify(pacientes));
+    leer();
+    alert("Registro eliminado correctamente!");
+}
---
+++ E7.TallerCRUDEPS/funcionesRegistro.js
@@ -0,0 +1,76 @@
+
+
+function cargarModal(){
+    document.getElementById("boton-modal").click();   
+    
+}
+
+function registrar(){
+    let form = document.getElementById("container-registro");
+    form.style.display = "block";
+    
+}
+
+document.getElementById("formulario").addEventListener("submit", crear);
+
+
+function crear(e){
+    // debugger;
+    document.getElementById("pageInicio").click();
+    let nombre = document.getElementById("inputNombre").value;
+    let apellido = document.getElementById("inputApe").value;
+    let tel = document.getElementById("inputTel").value;
+    let fecha = document.getElementById("inputFecha").value;
+    let email = document.getElementById("inputEmail").value;
+    let pass = document.getElementById("inputPass").value;
+
+    let user =  {
+        nombre, apellido, tel, fecha, email, pass
+    }
+  
+   if(localStorage.getItem("usuarios") === null){
+        let users = [];
+        users.push(user);
+        localStorage.setItem("usuarios", JSON.stringify(users));
+    } else{
+        let users = JSON.parse(localStorage.getItem("usuarios"));
+        let elementIndex = users.findIndex((obj)=>obj.email == email);
+        if(elementIndex == -1){
+            users.push(user);
+            localStorage.setItem("usuarios", JSON.stringify(users));
+        }else{
+            alert("Usuario existente, por favor digite otro correo.");
+        }
+      
+    }
+    
+    
+    document.getElementById("formulario").reset();
+    e.preventDefault();
+    
+    console.log("Usuarios creado Correctamente")
+}
+
+
+function login(){
+    let email = document.getElementById("loginEmail").value;
+    let pass = document.getElementById("loginPass").value;
+
+    let users = JSON.parse(localStorage.getItem("usuarios"));
+    let elementIndex = users.findIndex((obj)=>obj.email == email);
+    let elementIndexP = users.findIndex((obj)=>obj.pass == pass);
+    console.log(elementIndex);
+
+    if(elementIndex != -1 && pass != ""){
+
+        if(pass == users[elementIndex].pass){
+            document.getElementById("pageInicio").click();
+        }else{
+            alert("Usuario o contraseña incorrectos!");
+        }
+    }else{
+        alert("Usuario o contraseña incorrectos!");
+        
+    }
+}
+
